786c3bda5fb698ac031ee54cdc3418903cfa74c7
Paweł Ostromecki <pawel.ostromecky@gmail.com>
2024-05-09 21:19:33 +0200

    fix: Fix query params builder

--- src/app/weather/utils/http-params.ts
+++ src/app/weather/utils/http-params.ts
@@ -3,10 +3,10 @@ import {HttpParams} from "@angular/common/http";
 type Params = { [key: string]: string | number | boolean };
 
 export class HttpParamsBuilder<PType extends Params> {
-  htpParams: HttpParams = new HttpParams();
+  private htpParams: HttpParams = new HttpParams();
 
   set<K extends keyof PType>(key: K, value: PType[K]): HttpParamsBuilder<PType> {
-    this.htpParams.set(key as string, value);
+    this.htpParams = this.htpParams.set(key as string, value);
     return this;
   }
 
