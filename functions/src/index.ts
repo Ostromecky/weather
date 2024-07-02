5221a8e0e2bebcbc4c29d5abbf1b6f4cff363879
Paweł Ostromecki <pawel.ostromecky@gmail.com>
2024-07-02 13:54:10 +0200

    ref: Remove changes related to algolia search after integration

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,4 +3,3 @@ import 'reflect-metadata';
 export {getWeather} from './weather';
 export {getCity} from './city';
 export {getForecast} from './forecast';
-// export {addToIndex, deleteIndex, updateIndex} from './search';
